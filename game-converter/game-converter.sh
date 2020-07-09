6fe4c24ff36549249bb1b5c3cbe286545d6acf9a
Justme488 <justme488@gmail.com>
2020-07-09 02:09:25 -0400

    Update game-converter.sh

--- game-converter/game-converter.sh
+++ game-converter/game-converter.sh
@@ -1,25 +1,17 @@
 #!/usr/bin/env bash
-
-
 ##################################
-#  Created by Ed Houseman Jr     #            
-#                                #
+#  Created by Ed Houseman Jr     #
 #  email - Justme488@gmail.com   #
-#                                #
 #  discord - justme488#6127      #
-#                                #
 ##################################
-
 ##################################################################################################
 # In order to use this, you must have ciso, libsdl1.2debian, and zenity installed on your system #
 ##################################################################################################
-
-
 #############
 # Variables #
 #####################################################################################################################################################################################################################################
 
-# Define main variables 
+# Define main variables
 opt1="Single File Conversion"
 opt2="Batch File Conversion"
 opt3="Convert cue, gdi, or toc to chd"
@@ -48,7 +40,7 @@ opt_chdman5="chdman version 5 (0.221)"
 #####################################################################################################################################################################################################################################
 # Create a function for single file conversion to chd
 single_tochd () {
-  
+
   # Asks the user for input file, and creates $single_tochd_input
   single_tochd_input=$(zenity --file-selection --filename="Desktop" --title="Select a file to convert to chd - Must be cue, gdi, or toc")
 
@@ -65,7 +57,7 @@ single_tochd () {
 
   # Continue to select file output directory if valid filetype (cue,gdi,toc) creating variable "$single_tochd_output", and run the rest, or go to "else" in function
   if [[ "$single_tochd_input_basename" == *.cue ]] || [[ "$single_tochd_input_basename" == *.gdi ]] || [[ "$single_tochd_input_basename" == *.toc ]]; then
-    
+
     # Opens box asking which version of chdman, and sets variable "$chdman_ver" to "$chdman4" or "$chdman5"
     chdman_ver=$(zenity --list --height="200" --width="350" --title="Which version of chdman do you want to use?" --text="Which version of chdman?" --radiolist  --column="Pick" --column="chd version" FALSE "$opt_chdman4" FALSE "$opt_chdman5")
 
@@ -81,7 +73,7 @@ single_tochd () {
         exit
       fi
 
-    # Create final output path variable "$final_single_tochd_output"
+    # Create final output path
     final_single_tochd_output="$single_tochd_output"/"$single_tochd_input_basename_no_ext.chd"
 
     # Don't overwrite if file exists
@@ -121,7 +113,7 @@ single_tochd () {
 #####################################################################################################################################################################################################################################
 # Create a function for single file conversion to gdi
 single_chdtogdi () {
-  
+
   # Asks the user for input file, and creates $single_chdtogdi_input
   single_chdtogdi_input=$(zenity --file-selection --filename="Desktop" --title="Select chd file to convert to gdi")
 
@@ -130,16 +122,16 @@ single_chdtogdi () {
       exit
     fi
 
-  # Takes input variable "$single_chdtogdi_input" (path/game name.chd) , and removes the path creating "$single_chdtogdi_input_basename" (game name.chd) 
+  # Takes input variable "$single_chdtogdi_input" (path/game name.chd) , and removes the path creating "$single_chdtogdi_input_basename" (game name.chd)
   single_chdtogdi_input_basename="$(basename "$single_chdtogdi_input")"
 
   # Takes variable "$single_chdtogdi_input_basename", and removes the extention (.chd) creating "$single_chdtogdi_input_basename_no_ext" (game name)
   single_chdtogdi_input_basename_no_ext=${single_chdtogdi_input_basename%.*}
 
-  # Continue to select file output directory if valid filetype (chd) creating "single_chdtogdi_output" and run the rest, or go to "else" in function 
-  if [[ "$single_chdtogdi_input_basename" == *.chd ]]; then 
+  # Continue to select file output directory if valid filetype (chd) creating "single_chdtogdi_output" and run the rest, or go to "else" in function
+  if [[ "$single_chdtogdi_input_basename" == *.chd ]]; then
     single_chdtogdi_output=$(zenity --file-selection --directory --filename="Desktop" --title="Where do you want to save it? Folder will be created automatically.")
-  
+
     # Exits if user hits cancel button
     if [[ "$?" != 0 ]]; then
       exit
@@ -147,7 +139,8 @@ single_chdtogdi () {
 
     # If output directory doesn't exist
     if [[ ! -d "$single_chdtogdi_output"/"$single_chdtogdi_input_basename_no_ext" ]]; then
-      # Creates a directory for the conversion
+
+     # Creates a directory for the conversion
       mkdir "$single_chdtogdi_output/$single_chdtogdi_input_basename_no_ext"
 
       # Create final output path variable
@@ -183,13 +176,13 @@ single_chdtocue () {
       exit
     fi
 
-  # Takes input variable "$single_chdtocue_input" (path/game name.chd) , and removes the path creating "$single_chdtocue_input_basename" (game name.chd) 
+  # Takes input variable "$single_chdtocue_input" (path/game name.chd) , and removes the path creating "$single_chdtocue_input_basename" (game name.chd)
   single_chdtocue_input_basename="$(basename "$single_chdtocue_input")"
 
   # Takes variable "$single_chdtocue_input_basename", and removes the extention (.chd) creating "$single_chdtocue_input_basename_no_ext" (game name)
   single_chdtocue_input_basename_no_ext=${single_chdtocue_input_basename%.*}
 
-  # Continue to select file output directory if valid filetype (chd) creating "$single_chdtocue_output" and run the rest, or go to "else" in function 
+  # Continue to select file output directory if valid filetype (chd) creating "$single_chdtocue_output" and run the rest, or go to "else" in function
   if [[ "$single_chdtocue_input_basename" == *.chd ]]; then
     single_chdtocue_output=$(zenity --file-selection  --directory --filename="Desktop" --title="Where do you want to save it? Folder will be created automatically.")
 
@@ -199,6 +192,7 @@ single_chdtocue () {
 
     # Don't overwrite if folder exists
     if [[ ! -d "$single_chdtocue_output"/"$single_chdtocue_input_basename_no_ext" ]]; then
+    
       # Create a directory for the files to be extracted in
       mkdir "$single_chdtocue_output"/"$single_chdtocue_input_basename_no_ext"
 
@@ -210,13 +204,13 @@ single_chdtocue () {
 
         # Cancel conversion, and delete incomplete file if cancel is pressed.
         if [[ "$?" != 0 ]]; then
-          rm "$final_single_chdtocue_output"
+          rm -r "$final_single_chdtocue_output"
           pkill chdman5
         fi
     else
       # Display error box stating the output folder already exists
       zenity --error --width="400" --title="Folder already exists!" --text="$single_chdtocue_output/$single_chdtocue_input_basename_no_ext already exists!"
-    fi  
+    fi
   else
     # error pop up box if not valid filetype, and then start over function
     zenity --error --width="500" --height="200" --title="That file is not a chd file. Please try again." --text="$single_chdtocue_input_basename is not a chd file. Please try again." && single_chdtocue
@@ -240,8 +234,9 @@ single_isotocso () {
 
   # Takes "$single_isotocso_input_basename", and removes the extention (.iso) creating "$single_isotocso_input_basename_no_ext" (game name)
   single_isotocso_input_basename_no_ext=${single_isotocso_input_basename%.*}
-    
+
   if [[ "$single_isotocso_input_basename" == *.iso ]]; then
+    
     # Ask user for file output folder, and creates "$single_isotocso_output"
     single_isotocso_output=$(zenity --file-selection  --directory --filename="Desktop" --title="Where do you want to save it?")
 
@@ -270,9 +265,9 @@ single_isotocso () {
     else
       # Display error box stating the output file already exists
       zenity --error --width="400" --title="File already exists!" --text="$final_single_isotocso_output already exists!"
-    fi  
+    fi
   else
-    # error pop up box if not valid filetype, and then start over function  
+    # error pop up box if not valid filetype, and then start over function
     zenity --error --width="500" --height="200" --title="That file is not a iso file. Please try again." --text="$single_isotocso_input_basename is not a iso file. Please try again." && single_isotocso
   fi
 }
@@ -289,13 +284,14 @@ single_csotoiso () {
       exit
     fi
 
-  # Takes input variable "$single_csotoiso_input" (path/game name.cso) , and removes the path creating "$single_csotoiso_input_basename" (game name.cso) 
+  # Takes input variable "$single_csotoiso_input" (path/game name.cso) , and removes the path creating "$single_csotoiso_input_basename" (game name.cso)
   single_csotoiso_input_basename="$(basename "$single_csotoiso_input")"
 
   # Takes variable "$single_csotoiso_input_basename", and removes the extention (.cso) creating "$single_csotoiso_input_basename_no_ext" (game name)
   single_csotoiso_input_basename_no_ext=${single_csotoiso_input_basename%.*}
-    
-  if [[ $single_csotoiso_input_basename == *.cso ]]; then
+
+    if [[ $single_csotoiso_input_basename == *.cso ]]; then
+
     # Ask user for file output folder, and creates "$single_csotoiso_output"
     single_csotoiso_output=$(zenity --file-selection --directory --filename="Desktop" --title="Where do you want to save it?")
 
@@ -312,8 +308,8 @@ single_csotoiso () {
 
       # Start uncompressing .cso
       (ciso 0 "$single_csotoiso_input" "$final_single_csotoiso_output" | zenity --progress --auto-kill --pulsate --auto-close --title="Converting $single_csotoiso_input_basename_no_ext" --text="creating: $single_csotoiso_input_basename_no_ext.iso")
- 
-        # Cancel conversion, and delete incomplete file if cancel is pressed.
+
+        # Cancel conversion, and delete incomplete file if cancel is pressed
         if [[ "$?" != 0 ]]; then
           rm "$final_single_csotoiso_output"
           pkill ciso
@@ -322,7 +318,7 @@ single_csotoiso () {
       # Display error box stating the output file already exists
       zenity --error --width="400" --title="File already exists!" --text="$final_single_csotoiso_output already exists!"
     fi
-  else 
+  else
     zenity --error --width="500" --height="200" --title="That file is not a cso file. Please try again." --text="$single_csotoiso_input_basename is not a cso file. Please try again." && single_csotoiso
   fi
 }
@@ -357,6 +353,7 @@ batch_tochd () {
 
   # Loop through games folder recursively looking for cue, gdi, and toc files
   for batch_tochd_file in "$batch_tochd_input"/*/*.{cue,gdi,toc}; do
+
     # Removes path for file in loop, creating "$batch_chdtogdi_file_basename" (filename.chd)
     batch_tochd_file_basename="$(basename "$batch_tochd_file")"
 
@@ -434,7 +431,7 @@ batch_chdtogdi() {
 
       # Start converting to gdi
       (chdman5 extractcd -i "$batch_chdtogdi_file" -o "$final_batch_chdtogdi_output" | zenity --progress --pulsate --auto-kill --auto-close --title="Converting $batch_chdtogdi_file_basename_no_ext" --text="Creating: $batch_chdtogdi_file_basename_no_ext.gdi")
-    
+
         # Cancel conversion, and delete incomplete file if cancel is pressed.
         if [[ "$?" != 0 ]]; then
           rm -r "$batch_chdtogdi_output"/"$batch_chdtogdi_file_basename_no_ext"
@@ -467,13 +464,14 @@ batch_chdtocue() {
 
   # Loop through games folder recursively looking for chd files
   for batch_chdtocue_file in "$batch_chdtocue_input"/*.chd; do
+
     # Removes path for file in loop, creating "$batch_chdtocue_file_basename" (filename.chd)
     batch_chdtocue_file_basename="$(basename "$batch_chdtocue_file")"
 
     # Takes the basename "batch_chdtocue_file_basename", and removes extension (.chd) creating "$batch_chdtocue_file_basename_no_ext" (filename)
     batch_chdtocue_file_basename_no_ext=${batch_chdtocue_file_basename%.*}
 
-    # Create a smaller output path "$final_batch_chdtocue_output". (output path = output folder selected / input file basename with no extension / input file basename with no extension)
+    # Create final output path
     final_batch_chdtocue_output="$batch_chdtocue_output"/"$batch_chdtocue_file_basename_no_ext"/"$batch_chdtocue_file_basename_no_ext.cue"
 
     # Don't overwrite if folder exists
@@ -484,7 +482,6 @@ batch_chdtocue() {
 
       # Start converting to bin/cue
       (chdman5 extractcd -i "$batch_chdtocue_file" -o "$final_batch_chdtocue_output" | zenity --progress --pulsate --auto-kill --auto-close --title="converting $batch_chdtocue_file" --text="Creating: $batch_chdtocue_file_basename_no_ext.cue")
-    
         # Cancel conversion, and delete incomplete file if cancel is pressed.
         if [[ "$?" != 0 ]]; then
           rm -r "$batch_chdtocue_output"/"$batch_chdtocue_file_basename_no_ext"
@@ -492,7 +489,7 @@ batch_chdtocue() {
           break
         fi
     fi
-  done  
+  done
 }
 #######################################
 # Create a function for $opt2 & $opt6 #
@@ -540,7 +537,7 @@ batch_isotocso() {
 
         # Start converting to cso
         (ciso "$action4" "$batch_isotocso_file" "$final_batch_isotocso_output" | zenity --progress --auto-kill --pulsate --width="400" --auto-close --title="Converting $batch_isotocso_file_basename_no_ext" --text="Creating: $batch_isotocso_file_basename_no_ext.cso")
-    
+
           # Cancel conversion, and delete incomplete file if cancel is pressed.
           if [[ "$?" != 0 ]]; then
             rm "$final_batch_isotocso_output"
@@ -553,51 +550,54 @@ batch_isotocso() {
 #######################################
 # Create a function for $opt2 & $opt7 #
 #####################################################################################################################################################################################################################################
-
 # Create a function for batch cso to iso conversion
 batch_csotoiso() {
   # Asks the user for input folder, and creates "$batch_csotoiso_input"
   batch_csotoiso_input=$(zenity --file-selection --directory --filename="Desktop" --title="Select the folder where you keep your cso files")
 
-  # Exits if user hits cancel button
-  if [[ "$?" != 0 ]]; then
-    exit
-  fi
+    # Exits if user hits cancel button
+    if [[ "$?" != 0 ]]; then
+      exit
+    fi
 
   # Asks the user for output folder, and creates "$batch_csotoiso_output"
   batch_csotoiso_output=$(zenity --file-selection --directory --filename="Desktop" --title="Where you want to save them")
 
-  # Exits if user hits cancel button
-  if [[ "$?" != 0 ]]; then
-    exit
-  fi
+    # Exits if user hits cancel button
+    if [[ "$?" != 0 ]]; then
+      exit
+    fi
 
   # Loop through games folder recursively looking for cso files
   for batch_csotoiso_file in "$batch_csotoiso_input"/*.cso; do
+
     # Removes path for file in loop, creating "$batch_csotoiso_file_basename" (filename.cso)
     batch_csotoiso_file_basename="$(basename "$batch_csotoiso_file")"
 
     # Takes the basename "batch_csotoiso_file_basename", and removes extension (.cso) creating "$batch_csotoiso_file_basename_no_ext" (filename)
     batch_csotoiso_file_basename_no_ext=${batch_csotoiso_file_basename%.*}
 
+    # Create final output path
+    final_batch_csotoiso_output="$batch_csotoiso_output"/"$batch_csotoiso_file_basename_no_ext.iso"
+
     # Don't overwrite if file exists
-    if [[ ! -f "$batch_csotoiso_output"/"$batch_csotoiso_file_basename_no_ext.iso" ]]; then
+    if [[ ! -f "$final_batch_csotoiso_output" ]]; then
 
       # Start converting to iso
-      ciso 0 "$batch_csotoiso_file" "$batch_csotoiso_output"/"$batch_csotoiso_file_basename_no_ext.iso" | zenity --progress --auto-kill --pulsate --width="400" --auto-close --title="Converting cso to iso" --text="Creating $batch_csotoiso_file_basename_no_ext.iso"
+      (ciso 0 "$batch_csotoiso_file" "$final_batch_csotoiso_output" | zenity --progress --auto-kill --pulsate --width="400" --auto-close --title="Converting $batch_csotoiso_file" --text="Creating: $batch_csotoiso_file_basename_no_ext.iso")
+
+        # Cancel conversion, and delete incomplete file if cancel is pressed.
+        if [[ "$?" != 0 ]]; then
+          rm "$final_batch_csotoiso_output"
+          pkill ciso
+          break
+        fi
     fi
   done
-
-  # Exits if user hits cancel button
-  if [[ "$?" != 0 ]]; then
-    exit
-  fi
 }
-
-########  
+########
 # Main #
 #####################################################################################################################################################################################################################################
-
 # Opens box asking for user input, and sets variable $action1 to $opt1 or $opt2 from user selection
 action1=$(zenity --list --title="What kind of conversion?" --text="What kind of conversion?" --radiolist  --column="Pick" --column="Conversion Type" FALSE "$opt1" FALSE "$opt2")
 
@@ -609,12 +609,10 @@ fi
 # Opens box asking for user input, and sets variable $action2 to $opt3,$opt4,$opt5,$opt6, or $opt7 from user selection
 action2=$(zenity --list --height="300" --width="350" --title="How do you want to convert?" --text="How do you want to convert?" --radiolist  --column="Pick" --column="Conversion Type" FALSE "$opt3" FALSE "$opt4" FALSE "$opt5" FALSE "$opt6" FALSE "$opt7")
 
-#exits if user hits cancel button
-if [[ "$?" != 0 ]]; then
-  exit
-fi
-
-
+  #exits if user hits cancel button
+  if [[ "$?" != 0 ]]; then
+    exit
+  fi
 
 # If user selected "Single File Conversion", and " Convert to chd"
 if [[ "$action1" == "$opt1" ]] && [[ "$action2" == "$opt3" ]]; then
@@ -624,7 +622,7 @@ if [[ "$action1" == "$opt1" ]] && [[ "$action2" == "$opt3" ]]; then
 elif [[ "$action1" == "$opt1" ]] && [[ "$action2" == "$opt4" ]]; then
   single_chdtogdi
 
-# If user selected "Single File Conversion", and "Convert chd to bin/cue""
+# If user selected "Single File Conversion", and "Convert chd to bin/cue"
 elif [[ "$action1" == "$opt1" ]] && [[ "$action2" == "$opt5" ]]; then
   single_chdtocue
 
@@ -644,7 +642,7 @@ elif [[ "$action1" == "$opt2" ]] && [[ "$action2" == "$opt3" ]]; then
 elif [[ "$action1" == "$opt2" ]] && [[ "$action2" == "$opt4" ]]; then
   batch_chdtogdi
 
-# If user selected "batch Conversion", and "Convert chd to bin/cue""
+# If user selected "batch Conversion", and "Convert chd to bin/cue"
 elif [[ "$action1" == "$opt2" ]] && [[ "$action2" == "$opt5" ]]; then
   batch_chdtocue
 
@@ -662,5 +660,4 @@ if zenity --question --text "Do you have more files to convert?"; then
 else
   exit
 fi
-
 ###########################################################################################################################################################################################
